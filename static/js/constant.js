f403bbbe2a765b29941d356d4e7bf5ab50661ed5
SOMASAI DARRURU <76832603+somsai359@users.noreply.github.com>
2023-02-24 14:16:51 +0530

    Add files via upload

---
+++ static/js/constant.js
@@ -0,0 +1,18 @@
+let CONSTANT = {
+    UNASSIGNED: 0,
+    GRID_SIZE: 3,
+    BOX_SIZE: 1,
+    NUMBERS: [1,2,3],
+    LEVEL_NAME: [
+        'Easy',
+        'Medium',
+        'Hard',
+        'Very hard',
+        'Insane',
+        'Inhuman'
+    ],
+    GRID_SIZE_NAME:['3*3','6*6','9*9'],
+    LEVEL: [[1, 2, 3, 4, 5, 6],[4,8,12,16,20,24],[7,14,21,28,35,42]],
+
+    CUR_LEVEL:0
+}
